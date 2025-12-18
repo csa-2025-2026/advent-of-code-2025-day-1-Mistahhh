57149d601861f3f2d852d6397c03ebe243ce19e0
Khoi Huynh <118472546+Mistahhh@users.noreply.github.com>
2025-12-18 19:04:22 +0000

    done

--- Main.java
+++ Main.java
@@ -6,28 +6,39 @@ public class Main
 {
   public static void main(String[] args) throws IOException
   {
-    File file = new File(input.txt);  // this is your problem input as a File object
+    File file = new File("input.txt");  // this is your problem input as a File object
     Scanner sc = new Scanner(file);   // the Scanner object is now reading from the file object
-
-
+    int curNum = 50;
     int answer = 0;
     while (sc.hasNext())  // as long as the Scanner has more lines to read from the file...
     {
       String line = sc.nextLine();  // gets the next line of input.  This is like "R802"
 
       // TODO: Write code that gets the turning direction from the line.  This should be "L" or "R"
-
+      String direction = line.substring(0, 1);
 
       // TODO: Write code that gets a substring of the line that contains only the numbers
       // For R802, this would be a String variable with the value "802"
-
+      String numString = line.substring(1);
 
       // TODO: Using the parseInt method from the Integer class, convert the String "802" into an int
-
+      int num = Integer.parseInt(numString);
 
       // TODO: Now that you have the turning direction, and the turning amount, see if the turn results in a 0
+      
+      if (direction.equals("L"))
+      {
+        curNum -= num;
+      }
+      else if (direction.equals("R"))
+      {
+        curNum += num;
+      }
 
-
+      if ((curNum % 100) == 0)
+      {
+        answer++;
+      }
     }
 
     System.out.println("The password is " + answer);
